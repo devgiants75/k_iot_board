a801bc82aab5dd6bde32653e9da9ca1dbd358071
devgiants75 <devgiants75@naver.com>
2025-11-18 14:16:51 +0900

    board-front refactor

--- board-front/src/apis/auth/auth.api.ts
+++ board-front/src/apis/auth/auth.api.ts
@@ -2,25 +2,16 @@
 
 import type { LoginRequest, LoginResponse } from "@/types/auth.type";
 import { publicApi } from "../common/axiosInstance";
-import { API_ROUTES } from "../common/base.path";
 import type { ApiResponse } from "@/types/common/ApiResponse";
+import { AUTH_PATH } from "./auth.path";
 
 export const authApi = {
   login: async (req: LoginRequest): Promise<LoginResponse> => {
     // axios.메서드<메서드반환타입>();
     const res = await publicApi.post<ApiResponse<LoginResponse>>(
-      API_ROUTES.AUTH.LOGIN,
+      AUTH_PATH.LOGIN,
       req
     );
     return res.data.data;
   },
 };
-
-// 로그인 요청
-// export async function login(req: LoginRequest): Promise<LoginResponse> {
-//   const res = await publicApi.post<ApiResponse<LoginResponse>>(
-//     API_ROUTES.AUTH.LOGIN,
-//     req
-//   );
-//   return res.data.data;
-// }
