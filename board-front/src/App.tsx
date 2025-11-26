ec335f607667c9bd07e0130a13ffd23076da9d0f
devgiants75 <devgiants75@naver.com>
2025-11-26 11:39:31 +0900

    20251126 - Layout CSS

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,18 +1,15 @@
 import { useEffect } from "react";
-import LoginPage from "./pages/LoginPage";
 import { useAuthStore } from "./stores/auth.store";
 import { userApi } from "./apis/user/user.api";
-import { Link, Route, Routes } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import OAuth2CallbackPage from "./pages/OAuth2CallbackPage";
 import { GlobalStyle } from "./styles/global";
 import Layout from "./components/layout/Layout";
+import AuthRouter from "./pages/auth/AuthRouter";
+import MainRouter from "./pages/MainRouter";
 
 export default function App() {
   const { isInitialized, accessToken, user, setUser } = useAuthStore();
 
   useEffect(() => {
-    console.log(isInitialized);
     if (!isInitialized) return;
     if (!accessToken) return;
     if (user) return;
@@ -25,11 +22,10 @@ export default function App() {
         }
       }
     })();
-
   }, [isInitialized, accessToken]);
 
   if (!isInitialized) {
-    return <div>로딩중</div>
+    return <div>로딩중</div>;
   }
 
   const isLoggedIn = Boolean(accessToken && user);
@@ -37,9 +33,13 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <div>Main-Dashboard</div>
-      </Layout>
+      {isLoggedIn ? (
+        <Layout>
+          <MainRouter />
+        </Layout>
+      ) : (
+        <AuthRouter />
+      )}
     </>
   );
 }
